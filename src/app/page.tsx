cf50f8605a5522fb5c81d937c6337251c1f1120b
Daniel Shaby <dshaby@berkeley.edu>
2024-12-04 12:47:24 -0600

    adds background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,22 @@ import { HydrateClient } from "~/trpc/server";
 import MathSolver from "./_components/MathSolver";
 import Header from "./_components/Header";
 import "katex/dist/katex.min.css";
+import Image from "next/image";
 
 export default async function Home() {
   return (
     <HydrateClient>
-      <Header />
-      <MathSolver />
+      <div className="relative min-h-screen w-full">
+        <Image
+          src="/images/background-img.jpg"
+          alt="background image"
+          fill
+          className="z-[-1] object-cover"
+          priority
+        />
+        <Header />
+        <MathSolver />
+      </div>
     </HydrateClient>
   );
 }
